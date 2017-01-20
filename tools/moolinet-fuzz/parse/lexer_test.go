bf7bd4656cd6cd926b1ba7417a246fd1d580c9e2
Lesterpig <git@lesterpig.com>
2017-01-20 16:22:54 +0100

    Improve unit tests of lexer.go

--- tools/moolinet-fuzz/parse/lexer_test.go
+++ tools/moolinet-fuzz/parse/lexer_test.go
@@ -1,25 +1,45 @@
 package parse
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
 func TestGrammar(t *testing.T) {
-	testCases := []string{
-		"[int] | [int -100,0xff] | [int 1,2]2",
-		"[loop 5,10][int],[/loop][int]",
-		"Good [enum afternoon,night][enum  Sir, Madam, my dearest friend,],",
+	type tc struct {
+		name, grammar, regex string
 	}
 
-	for _, c := range testCases {
-		g, err := NewGrammar(c)
-		if err != nil {
-			t.Fatal(err)
-		}
+	testCases := []tc{
+		{"integer", "[int] | [int -100,0xff] | [int 1,2]2", `\d+ \| -?\d+ \| 12`},
+		{"loop", "[loop 5,10][int],[/loop][int]", `(-?\d+,){5,9}-?\d+`},
+		{"enum", "Good [enum afternoon,night][enum  Sir, Madam, my dearest friend,],", `Good (afternoon|night)( Sir| Madam| my dearest friend)?`},
+	}
 
-		r, err := g.Render()
-		if err != nil {
-			t.Fatal(err)
-		}
+	for i, c := range testCases {
+		func(i int, c tc) { // closure
+			t.Run(c.name, func(t *testing.T) {
+				g, err := NewGrammar(c.grammar)
+				if err != nil {
+					t.Error("Unable to compile grammar in case", i, ":", err)
+					return
+				}
 
-		t.Logf("%s", r)
+				reg := regexp.MustCompile(c.regex)
+
+				for j := 0; j < 1000; j++ {
+					r, err := g.Render()
+					if err != nil {
+						t.Error("Unable to generate test case in case", i, ":", err)
+						break
+					}
+
+					if !reg.Match(r) {
+						t.Error("Invalid result for case", i, ":", string(r))
+						break
+					}
+				}
+			})
+		}(i, c)
 	}
 }
