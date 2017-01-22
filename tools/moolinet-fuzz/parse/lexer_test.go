99465d8987b6347564f1905e30b6befd6eab9da5
Lesterpig <git@lesterpig.com>
2017-01-22 14:22:19 +0100

    Improve test coverage and error management (fuzz)

--- tools/moolinet-fuzz/parse/lexer_test.go
+++ tools/moolinet-fuzz/parse/lexer_test.go
@@ -8,18 +8,46 @@ import (
 func TestGrammar(t *testing.T) {
 	type tc struct {
 		name, grammar, regex string
+		err                  bool
 	}
 
 	testCases := []tc{
-		{"integer", "[int] | [int -100,0xff] | [int 1,2]2", `\d+ \| -?\d+ \| 12`},
-		{"loop", "[loop 5,10][int],[/loop][int]", `(-?\d+,){5,9}-?\d+`},
-		{"enum", "Good [enum afternoon,night][enum  Sir, Madam, my dearest friend,],", `Good (afternoon|night)( Sir| Madam| my dearest friend)?`},
+		// Standard cases
+		{"integer", "[int] | [int -100,0xff] | [int 10] | [int 1,2]2", `-?\d+ \| -?\d+ \| \d \| 12`, false},
+		{"loop", "[loop 5,10][int],[/loop][int]", `(-?\d+,){5,9}-?\d+`, false},
+		{"enum", "Good [enum afternoon,night][enum  Sir, Madam, my dearest friend,],", `Good (afternoon|night)( Sir| Madam| my dearest friend)?`, false},
+		{"newline", "[int 10]\n[int 10,100]", `\d\n\d\d`, false},
+		// Syntax error for integers
+		{"err_int_0_no_space", "[int", "", true},
+		{"err_int_0_other", "[int\n", `\n`, false},
+		{"err_int_0_with_space", "[int ", "", true},
+		{"err_int_1", "[int _]", "", true},
+		{"err_int_2", "[int _,_]", "", true},
+		{"err_int_3", "[int 0,1,2]", "", true},
+		{"err_int_float", "[int 0.5]", "", true},
+		{"err_int_exp", "[int 1e5]", "", true},
+		// Syntax error for loops
+		{"err_loop_unclosed", "[loop]", "", true},
+		{"err_loop_unclosed_2", "[loop][loop][/loop]", "", true},
+		{"err_loop_unopened", "[/loop]", "", true},
+		// Syntax error for enumerations
+		{"err_enum_space", "[enum ", "", true},
+		{"err_enum_none", "[enum ]", "", true},
+		// Misc errors
+		{"err_tmpl", "{{ invalid }}", "", true},
 	}
 
 	for i, c := range testCases {
 		func(i int, c tc) { // closure
 			t.Run(c.name, func(t *testing.T) {
 				g, err := NewGrammar(c.grammar)
+				if c.err {
+					t.Log(err)
+					if err == nil {
+						t.Error("Expected error in case", i)
+					}
+					return
+				}
 				if err != nil {
 					t.Error("Unable to compile grammar in case", i, ":", err)
 					return
